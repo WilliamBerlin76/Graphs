3f1a6e56c617351fdda7de7607ed80138f2a85af
WilliamBerlin76 <william.berlin76@gmail.com>
2020-03-17 17:13:33 -0500

    completed first pass of ancestor problem

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,9 +1,6 @@
-# class Gr_Node():
-#     def __init__(self, value):
-#         self.value = value
-#         self.parents = set()
-#         self.children = set()
-
+import sys
+sys.path.append('../graph')
+from util import Stack
 
 # Create a graph and node class
 # dictionary of vertices, values are lists of 2 sets
@@ -52,19 +49,65 @@ class Anc_Graph():
             self.add_vertex(v2)
             self.vertices[v1][0].add(v2)
             self.vertices[v2][1].add(v1)
-my_gr = Anc_Graph()
+    def get_parents(self, v):
+        return self.vertices[v][0]
+# my_gr = Anc_Graph()
 
-my_gr.add_vertex(75)
-my_gr.add_child(75, 32)
-my_gr.add_child(32, 1)
-my_gr.add_child(32, 2)
-my_gr.add_child(100, 75)
-my_gr.add_parent(32, 88)
-my_gr.add_parent(75, 88)
-print(my_gr.vertices)
-# seems to be single direction, parents to children??
-# store parent node, with a set/list of children nodes?
-# adding nodes requires 2 args, (parent, child)
+# my_gr.add_vertex(75)
+# my_gr.add_child(75, 32)
+# my_gr.add_child(32, 1)
+# my_gr.add_child(32, 2)
+# my_gr.add_child(100, 75)
+# my_gr.add_parent(32, 88)
+# my_gr.add_parent(75, 88)
+# print(my_gr.vertices)
+
+
 # parents can share children
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    anc_tree = Anc_Graph()
+    # fill graph
+    for i in range(len(ancestors)):
+        anc_tree.add_child(ancestors[i][0], ancestors[i][1])
+
+    # account for no ancestors
+    if anc_tree.get_parents(starting_node) == set():
+        return -1
+    # create visited set
+    visited = set()
+    # longest_path to keep track of path
+    longest_path = [starting_node]
+    # dup_paths to account for duplicate seperate longest paths
+    dup_paths = [[longest_path]]
+    # count to keep track of the longest path
+    # longest_path_len = len(longest_path)
+    s = Stack()
+    s.push([starting_node])
+    while s.size() > 0:
+        new_path = s.pop()
+        last_node = new_path[-1]
+        if last_node not in visited:
+            visited.add(last_node)
+            if len(new_path) > len(longest_path):
+                longest_path = new_path
+                dup_paths = [longest_path]
+            elif len(new_path) == len(longest_path):
+                dup_paths.append(new_path)
+            for parent in anc_tree.get_parents(last_node):
+                path_copy = new_path.copy()
+                if parent is not set():
+                    path_copy.append(parent)
+                    s.push(path_copy)
+    # check for duplicate max paths
+    if len(dup_paths) > 1:
+        low_id = float('inf')
+        for i in range(len(dup_paths)):
+            if dup_paths[i][-1] < low_id:
+                low_id = dup_paths[i][-1]
+        return low_id
+    # if no duplicates, return the last index of longest path
+    return longest_path[-1]
+
+    # print(anc_tree.vertices)
+
+# print(earliest_ancestor([(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)], 9))
